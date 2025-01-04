56f43cde5b398aa1617e16e8cc408ab01111c4ed
Praveen Kumar T <73185062+tpraveen5@users.noreply.github.com>
2025-01-05 02:04:29 +0530

    Initial Commit

---
+++ ClimateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimateApp
+//
+//  Created by Talari Praveen kumar on 05/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
